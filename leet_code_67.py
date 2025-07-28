5b42b27a73c080629b445b66ac7a6129f678f7ed
Jubayer <shahriearjubayer199@gmail.com>
2025-07-28 21:40:56 +0600

    67 num problem is done

---
+++ leet_code_67.py
@@ -0,0 +1,25 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        store_1 = int(a,base=2)
+        store_2 = int(b,base=2)
+        sum = bin(store_1+ store_2 )
+        sum = str(sum)
+        result = sum[2:]
+        return result
+
+    
+s = Solution()
+a = "1010"
+b = "1011"
+print(s.addBinary(a,b))
+        
+
+
+
+
+
+
+
+
+        
+        
