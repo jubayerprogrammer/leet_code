fcc4162b9f299559086467a0eddd6e6ada8656a9
Jubayer <shahriearjubayer199@gmail.com>
2025-07-31 16:51:04 +0600

    179 number problem is solved

--- leet_code_179.py
+++ leet_code_179.py
@@ -1,10 +1,29 @@
+from typing  import List
+from functools import cmp_to_key
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        result = x**n
-        
-        return result
+    def compare(self,a,b):
+        if(a+b>b+a):
+            return -1
+        elif(b+a>a+b):
+            return 1
+        else:
+            return 0
 
-s = Solution()    
-x = 2.00000
-n= -2
-print(s.myPow(x,n))
+
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str,nums))
+        nums.sort(key=cmp_to_key(self.compare))
+
+        result = ""
+        for i in nums:
+            result+= i
+
+        if(result[0] == "0"):
+            return result[0]
+        else:
+            return result
+
+
+s = Solution()
+nums = [0,0]
+print(s.largestNumber(nums))
